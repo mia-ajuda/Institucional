8836bf2047df84216a3f8da49f2544c95632cb8d
EsioFreitas <esio.gustavo@gmail.com>
2020-04-25 14:44:05 -0300

    Add global styles

--- src/routes.js
+++ src/routes.js
@@ -8,7 +8,7 @@ export default function Routes() {
   return (
     <BrowserRouter>
       <Switch>
-        <Route path="/"  component={Home} />
+        <Route path="/" component={Home} />
       </Switch>
     </BrowserRouter>
   );
