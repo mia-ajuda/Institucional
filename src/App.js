6bff4284cf5b03449894bd9f30fd8514eda62760
EsioFreitas <esio.gustavo@gmail.com>
2020-05-01 13:07:51 -0300

    Arrumando router

--- src/App.js
+++ src/App.js
@@ -1,18 +1,19 @@
-import React from "react";
+import React from 'react';
 
-import NavBar from './components/NavBar'
-import Routes from "./routes";
-import GlobalStyle from "./assets/styles/global";
-import Footer from "./components/Footer";
+import NavBar from './components/NavBar';
+import Routes from './routes';
+import GlobalStyle from './assets/styles/global';
+import Footer from './components/Footer';
+import { BrowserRouter } from 'react-router-dom';
 
 function App() {
   return (
-    <>
+    <BrowserRouter>
       <NavBar />
       <Routes />
-      <Footer/>
+      <Footer />
       <GlobalStyle />
-    </>
+    </BrowserRouter>
   );
 }
 
--- src/routes.js
+++ src/routes.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import { Switch, Route } from 'react-router-dom';
 import Home from './pages/home';
 import Team from './pages/team';
 import Contact from './pages/contact';
@@ -7,13 +7,11 @@ import Support from './pages/support';
 
 export default function Routes() {
   return (
-    <BrowserRouter>
-      <Switch>
-        <Route path='/' exact component={Home} />
-        <Route path='/team' component={Team} />
-        <Route path='/contact' component={Contact} />
-        <Route path='/support' component={Support} />
-      </Switch>
-    </BrowserRouter>
+    <Switch>
+      <Route path='/' exact component={Home} />
+      <Route path='/team' component={Team} />
+      <Route path='/contact' component={Contact} />
+      <Route path='/support' component={Support} />
+    </Switch>
   );
 }
