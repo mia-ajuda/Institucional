5f6bc0b5d10c07c411e24f703b1f2dc590d71369
Arthur-Matos <git.arthurmatos@gmail.com>
2021-07-23 17:59:48 -0300

    #FIX - Update project members
    Co-authored-by: Brian Pina <brian.pina2001@gmail.com>

--- src/utils/members.js
+++ src/utils/members.js
@@ -3,7 +3,7 @@ import eduardo from '../assets/images/membros/eduardo.jpg';
 import hugo from '../assets/images/membros/hugo.jpg';
 import ivan from '../assets/images/membros/ivan.jpg';
 import jobert from '../assets/images/membros/jobert.jpg';
-import junior from '../assets/images/membros/junior.jpg';
+import junior from '../assets/images/membros/junior.png';
 import leo from '../assets/images/membros/leo.jpg';
 import lucas from '../assets/images/membros/lucas.jpg';
 import luis from '../assets/images/membros/luis.jpg';
@@ -23,6 +23,19 @@ import Danillo from '../assets/images/membros/danillo.jpg';
 import DudaMelo from '../assets/images/membros/dudaMelo.png';
 import Joao from '../assets/images/membros/joao.jpg';
 import PedroLima from '../assets/images/membros/pedroLima.jpg';
+import saleh from '../assets/images/membros/saleh.jpeg';
+import rafael from '../assets/images/membros/rafael.jpg';
+import lucasAmoedo from '../assets/images/membros/lucasAmoedo.jpg';
+import thiagoPaiva from '../assets/images/membros/thiagoPaiva.jpg';
+import rodrigoBalbino from '../assets/images/membros/rodrigoBalbino.jpg';
+import daviMarinho from '../assets/images/membros/daviMarino.jpg';
+import ithaloAzevedo from '../assets/images/membros/ithaloAzevedo.jpg';
+import brianPina from '../assets/images/membros/brianPina.jpg';
+import arthurMatos from '../assets/images/membros/arthurMatos.jpg';
+import gabrielSabanai from '../assets/images/membros/gabrielSabanai.jpg';
+import nataliaMagela from '../assets/images/membros/nataliaMagela.jpg';
+import andressaFernanda from '../assets/images/membros/andressaFernanda.jpg';
+
 
 export const currentMembers = [
   {
@@ -33,33 +46,42 @@ export const currentMembers = [
     name: 'Prof Maurício Serrano',
     image: Mauricio,
   },
-
   {
     name: 'Prof Fernando Cruz',
     image: Fernando,
+  },
+  {
+    name: 'Andressa Fernanda',
+    image: andressaFernanda,
+  },
+  {
+    name: 'Arthur Matos',
+    image: arthurMatos,
+  },
+  {
+    name: 'Brian Pina',
+    image: brianPina,
+  },
+  {
+    name: 'Davi Marinho',
+    image: daviMarinho,
   },
   {
     name: 'Dâmaso Pereira',
     image: junior,
   },
   {
-    name: 'Danillo Souza',
-    image: Danillo,
-  },
-  {
-    name: 'Duda Melo',
-    image: DudaMelo,
-  },
-
-  {
-    name: 'Ésio Freitas',
-    image: esio,
-  },
-  {
     name: 'Gabriel Pereira',
     image: Gabriel,
   },
-
+  {
+    name: 'Gabriel Sabanai',
+    image: gabrielSabanai,
+  },
+  {
+    name: 'Ithalo Azevedo',
+    image: ithaloAzevedo,
+  },
   {
     name: 'Ivan Diniz',
     image: ivan,
@@ -68,27 +90,45 @@ export const currentMembers = [
     name: 'João Pedro',
     image: Joao,
   },
-
   {
-    name: 'Leonardo Santos',
-    image: LeoSantos,
+    name: 'Lucas Amoêdo',
+    image: lucasAmoedo,
   },
-
+  {
+    name: 'Natália Magela',
+    image: nataliaMagela,
+  },
   {
     name: 'Pedro Féo',
     image: Pedro,
   },
   {
-    name: 'Pedro Lima',
-    image: PedroLima,
+    name: 'Rafael Cleydson',
+    image: rafael,
   },
   {
-    name: 'Welison Regis',
-    image: welisson,
+    name: 'Rodrigo Balbino',
+    image: rodrigoBalbino
   },
+  {
+    name: 'Saleh Kader',
+    image: saleh,
+  },
+  {
+    name: 'Thiago Paiva',
+    image: thiagoPaiva,
+  }
 ];
 
 export const oldMembers = [
+  {
+    name: 'Danillo Souza',
+    image: Danillo,
+  },
+  {
+    name: 'Duda Melo',
+    image: DudaMelo,
+  },
   {
     name: 'Elias Bernardo',
     image: bernado,
@@ -100,6 +140,10 @@ export const oldMembers = [
   {
     name: 'Eduardo Ribeiro',
     image: EduardoLima,
+  },
+  {
+    name: 'Ésio Freitas',
+    image: esio,
   },
   {
     name: 'Guilherme Rosa',
@@ -122,6 +166,10 @@ export const oldMembers = [
     image: leo,
   },
   {
+    name: 'Leonardo Santos',
+    image: LeoSantos,
+  },
+  {
     name: 'Lucas Souza',
     image: lucas,
   },
@@ -132,5 +180,13 @@ export const oldMembers = [
   {
     name: 'Matheus Estanislau',
     image: matheus,
+  },
+  {
+    name: 'Pedro Lima',
+    image: PedroLima,
+  },
+  {
+    name: 'Welison Regis',
+    image: welisson,
   },
 ];
